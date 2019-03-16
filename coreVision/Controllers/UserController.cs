0d7ffa5654e82ac9f1e56e206d078ff74a4950f7
ofir <gennin@gmail.com>
2019-03-16 14:31:04 +0200

    added validation for all fields

---
+++ coreVision/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using coreVision.Models;
+
+namespace coreVision.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add(int id = 0)
+        {
+            LandingPage landingPage = new LandingPage();
+            return View(landingPage);
+        }
+
+        [HttpPost]
+        public ActionResult Add(LandingPage landingPage)
+        {
+
+            return View("Add", new LandingPage());
+        }
+    }
+}
